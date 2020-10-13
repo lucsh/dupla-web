0ff6529889ed4d271d2c80d358603f1082cce269
Martin Moreira <mrtnmrr@gmail.com>
2020-10-13 16:37:18 -0300

    if not accept rotation, go timer

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,15 @@
-const holographicElement = document.getElementById("holographic");
+const holographicElement = document.getElementById('holographic');
 
-let deviceMotionEventPermissionGranted = false;
+const deviceMotionEventPermissionGranted = false;
 
 function updateHolographicBackground(value, rotation = 0) {
   const percentage = value * 100;
-  holographicElement.style.backgroundPosition = percentage + "%";
-  if (rotation){
-    holographicElement.style.mozTransform    = `rotate(${rotation}deg)`;
-    holographicElement.style.msTransform     = `rotate(${rotation}deg)`;
-    holographicElement.style.oTransform      = `rotate(${rotation}deg)`;
-    holographicElement.style.transform       = `rotate(${rotation}deg)`;
+  holographicElement.style.backgroundPosition = `${percentage}%`;
+  if (rotation) {
+    holographicElement.style.mozTransform = `rotate(${rotation}deg)`;
+    holographicElement.style.msTransform = `rotate(${rotation}deg)`;
+    holographicElement.style.oTransform = `rotate(${rotation}deg)`;
+    holographicElement.style.transform = `rotate(${rotation}deg)`;
   }
 }
 
@@ -26,33 +26,50 @@ function handleDeviceOrientation(event) {
   updateHolographicBackground(value, rotation);
 }
 
+function updateClock() {
+  const now = new Date();
+  // const milli = now.getMilliseconds() / 10;
+  const milli =
+    parseInt(`${`${now.getSeconds()}`.slice(-1)}${`00${now.getMilliseconds()}`.slice(-3)}`, 10) /
+    100;
+  let valor;
+  console.log(milli);
+  if (milli < 50) {
+    valor = milli * 2;
+  } else {
+    valor = 100 - (milli * 2 - 100);
+  }
+  holographicElement.style.backgroundPosition = `${valor}%`;
+}
+
+function initClock() {
+  updateClock();
+  setInterval(function () {
+    updateClock();
+  }, 10);
+}
+
 function handleTouchend() {
   if (!deviceMotionEventPermissionGranted) {
     // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === "function") {
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
       DeviceOrientationEvent.requestPermission()
         .then((permissionState) => {
-          if (permissionState === "granted") {
-            window.addEventListener(
-              "deviceorientation",
-              handleDeviceOrientation,
-              true
-            );
-          } // TODO else timer
+          if (permissionState === 'granted') {
+            window.addEventListener('deviceorientation', handleDeviceOrientation, true);
+          } else {
+            initClock();
+          }
         })
         .catch(console.error);
     } else {
-      window.addEventListener(
-        "deviceorientation",
-        handleDeviceOrientation,
-        true
-      );
+      window.addEventListener('deviceorientation', handleDeviceOrientation, true);
     }
   }
 }
 
 if (window.DeviceOrientationEvent) {
-  holographicElement.addEventListener("touchend", handleTouchend);
+  holographicElement.addEventListener('touchend', handleTouchend);
 }
 
-holographicElement.addEventListener("mousemove", handleMouseMove, true);
+holographicElement.addEventListener('mousemove', handleMouseMove, true);
